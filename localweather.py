52cf72dc48023fe39cf4054dad0412481badd789
Fábio Borges <fabioborges91@gmail.com>
2016-11-19 19:47:56 +0000

    Reviewed code; Gets location by IP; Weather API changed to yahoo; Send quote of the day as the boy of the msg and the weather as title; Created functions for the code;

--- localweather.py
+++ localweather.py
@@ -1,35 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import urllib2
 import json
 import re
 from pushbullet import Pushbullet
 
-#get location by IP 
-url = 'https://freegeoip.net/json/' 
-response = urllib2.urlopen (url)
-data = json.load (response)
-city = data['region_name']
-city = "Lisboa"
-
 #get weather for the current location on OpenWeatherMap
-response = urllib2.urlopen ('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',uk&appid=bd82977b86bf27fb59a04b61b657fb6f')
-data = json.load (response)
+def GetYahooWeather(city, country):
+    yahooWeather = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city+"%2C%20"+country+"%22)%20and%20u%3D%27c%27&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+    response = urllib2.urlopen (yahooWeather)
+    data = json.load(response)
 
-main = data['main']
-temp = str(main['temp'] - 273) + "C"
-humidity = str(main['humidity'])
-wind = str(data['wind']['speed'])
-clouds = str(data['clouds']['all'])
+    yahooChannel = data['query']['results']['channel']
+    yahooItem = yahooChannel['item']
+    yahooWeatherText = yahooItem['condition']['text']
+    yahooWeatherTemp = yahooItem['condition']['temp'] + u'\N{DEGREE SIGN}' + yahooChannel['units']['temperature']
+    title = city + " " + yahooWeatherTemp + " sky is " + yahooWeatherText
+    return title
 
-weather = data['weather']
-description = weather[0]['description']
+#get inspirational quote of the day
+def GetQuoteOfTheDay():
+    response = urllib2.urlopen('http://quotes.rest/qod.json')
+    data = json.load(response)
 
-Title = "Weather in " + city + " " + description + " " + temp
-Body = "Clouds " + clouds + "\n" + "Humidity is " + humidity + "\n" + "Wind " + wind
-
-print Title
-print Body
+    quotes = data['contents']['quotes'][0]
+    quoteOfTheDay = quotes['quote'] + ' - ' + quotes['author']
+    return quoteOfTheDay
 
 #send notification to pushbullet
-api_key = "API_KEY"
-pb = Pushbullet(api_key)
-pb.push_note(Title, Body)
+def SendPushBullet (title, body):
+    api_key = "_INSERT_YOUR_API_HERE_"
+    pb = Pushbullet(api_key)
+    pb.push_note(title, body)
+
+def main():
+    #get location by IP
+    url = 'https://freegeoip.net/json/'
+    response = urllib2.urlopen (url)
+    data = json.load (response)
+    city = data['region_name']
+    country = data['country_code']
+
+    weather = GetYahooWeather(city, country)
+    quoteOfTheDay = GetQuoteOfTheDay()
+    SendPushBullet(weather, quoteOfTheDay)
+
+if __name__ == "__main__":
+    main()
